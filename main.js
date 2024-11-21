8ff008b7816f2caa25107ec593ec850403c242a7
doaarashad <doaadod551@gmail.com>
2024-11-21 17:58:10 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+const arrayOfQuotes = [
+    {'author': 'Oscar Wilde', 
+     'quote': 'Be yourself; everyone else is already taken.'
+    },
+    {'author': 'Frank Zappa', 
+     'quote': 'So many books, so little time.'
+    },
+    {'author': 'Frank Sinatra', 
+     'quote': 'The best revenge is massive success.'
+    },
+    {'author': 'Marcus Tullius Cicero', 
+     'quote': 'A room without books is like a body without a soul.'
+    },
+    {'author': 'Mae West', 
+     'quote': 'You only live once, but if you do it right, once is enough.'
+    },
+    {'author': 'Elbert Hubbard', 
+     'quote': 'A friend is someone who knows all about you and still loves you.'
+    },
+];
+
+
+function generateQuote(){
+    const random = Number.parseInt(Math.random()*arrayOfQuotes.length + 1);
+    document.querySelector('#quoteOutput').textContent = `\"${arrayOfQuotes[random].quote}\"`;
+    document.querySelector('#authorOutput').textContent = `--${arrayOfQuotes[random].author}`;
+    
+}
